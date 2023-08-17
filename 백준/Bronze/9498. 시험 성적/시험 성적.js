75d9583ed371132916979ba91118f56f3caab00e
Jay <limithyunii@gmail.com>
2023-08-17 13:08:10 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+if (90 <= input && input <= 100) {
+  console.log("A");
+} else if (80 <= input && input <= 89) {
+  console.log("B");
+} else if (70 <= input && input <= 79) {
+  console.log("C");
+} else if (60 <= input && input <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
