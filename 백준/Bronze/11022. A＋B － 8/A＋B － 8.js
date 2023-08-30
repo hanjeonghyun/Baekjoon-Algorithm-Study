f6b68dfe7c6cce8357244d0b4f517580bf516178
Jay <limithyunii@gmail.com>
2023-08-30 23:36:01 +0900

    [Bronze V] Title: A+B - 8, Time: 152 ms, Memory: 9520 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,11 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const a = parseInt(input[0]);
+
+for (let b = 1; b <= a; b++) {
+  const number = input[b].split(" ");
+  const num1 = parseInt(number[0]);
+  const num2 = parseInt(number[1]);
+  const sum = num1 + num2;
+  console.log(`Case #${b}: ${num1} + ${num2} = ${sum}`);
+}
