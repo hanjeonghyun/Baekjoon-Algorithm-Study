369f05c080fbe8d63e7cc8edac7d1bd3352006c9
Jeonghyun <limithyunii@gmail.com>
2024-01-13 21:49:05 +0900

    [level Lv.0] Title: A 강조하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,11 @@
+function solution(myString) {
+    let answer = '';
+    for (let i = 0; i < myString.length; i++) {
+        if (myString[i].toLowerCase() === 'a') {
+            answer += 'A';
+        } else {
+            answer += myString[i].toLowerCase();
+        }
+    }
+    return answer;
+}
