1a966fcb0ec1bf86555434b9e8dcbb97ea80b4eb
Jeonghyun <limithyunii@gmail.com>
2024-04-04 23:54:15 +0900

    [level 0] Title: 코드 처리하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,16 @@
+function solution(code) {
+    let answer = '';
+    let mode = 0;
+    for (let i = 0; i < code.length; i++) {
+        if (code[i] === "1") {
+            mode = mode === 0 ? 1 : 0
+        } else if (code[i] !== "1" && i % 2 === mode) {
+            answer += code[i];
+        }
+    }
+    if (answer === "") {
+        return "EMPTY";
+    } else {
+        return answer;
+    }
+}
