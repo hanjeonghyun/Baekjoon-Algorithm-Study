f7306ac4aabe39c0936bb47b4fa59c637585be30
Jeonghyun <limithyunii@gmail.com>
2024-04-19 01:05:47 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,10 +1,3 @@
 function solution(my_string) {
-    var answer = [];
-    let result = my_string.split(' ');
-    for (let i=0;i<result.length;i++) {
-        if (result[i].length != 0) {
-            answer.push(result[i]);
-        }
-    }    
-    return answer;
+    return my_string.split(' ').filter(v => v);
 }
