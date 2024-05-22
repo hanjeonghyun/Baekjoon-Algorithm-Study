b893a61420bdcfb23bc8fc7e7063e52fba3b1d77
Jeonghyun <limithyunii@gmail.com>
2024-05-22 23:55:31 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,18 @@
+function solution(n, slicer, num_list) {
+    var answer = [];
+    let [a, b, c] = slicer;
+    
+    if (n == 1) {
+        answer = num_list.slice(0, b + 1);
+    } else if (n == 2) {
+        answer = num_list.slice(a);
+    } else if (n == 3) {
+        answer = num_list.slice(a, b + 1);
+    } else if (n == 4) {
+        for (let i = a; i < b + 1; i += c) {
+            answer.push(num_list[i]);
+        }
+    }
+    
+    return answer;
+}
