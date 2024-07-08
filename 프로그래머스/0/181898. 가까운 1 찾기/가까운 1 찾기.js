bc40b545badde12cc41bca2f40a8f0fd6eb8abbd
Jeonghyun <limithyunii@gmail.com>
2024-07-08 23:31:22 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,9 @@
+function solution(arr, idx) {
+    for (let i = idx; i < arr.length; i++) {
+        const cur = arr[i];
+        if (cur === 1) {
+            return i;
+        }
+    }
+    return -1;
+}
