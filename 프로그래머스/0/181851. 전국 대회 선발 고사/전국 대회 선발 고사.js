ed5e6df5aefa34097be8cbdf268148c79c7d0912
Jeonghyun <limithyunii@gmail.com>
2024-07-19 00:03:44 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,6 +1,12 @@
 function solution(rank, attendance) {
-    const deleted = rank.filter((_,index) => attendance[index]);
-    deleted.sort((a, b) => a - b);
-    const realPart = deleted.map(item => rank.findIndex(r => r === item));
-    return realPart[0] * 10000 + realPart[1] * 100 + realPart[2];
+    var answer = [];
+    let result = 0;
+    for (let i = 0; i < rank.length + 1; i++) {
+        if (attendance[i] == true) {
+        answer.push(rank[i])
+        }
+        answer.sort((a, b) => a - b);
+    }
+    result = 10000 * rank.indexOf(answer[0]) + 100 * rank.indexOf(answer[1]) + rank.indexOf(answer[2])
+    return result;
 }
