3ca850f8d4a04b0a49fec3a8fdfaf05155a8115d
Jeonghyun <limithyunii@gmail.com>
2024-08-16 11:06:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  return s
+    .split(" ")
+    .map((word) =>
+      word
+        .split("")
+        .map((char, idx) =>
+          idx % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+        )
+        .join("")
+    )
+    .join(" ");
+}
+
