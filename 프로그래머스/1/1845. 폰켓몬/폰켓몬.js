75cf21e458029130cd63f47e314e325e4ee6a588
Jeonghyun <limithyunii@gmail.com>
2024-09-02 23:39:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.92 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,10 @@
 function solution(nums) {
-    var answer = 0;
+    var answer = [...new Set(nums)];
+    let num = nums.length / 2;
+    if (num > answer.length) {
+        return answer.length;
+    } else {
+        return num;
+    }
     return answer;
 }
