779b4a538236d1c3614a9fa75b9d3b672f827e1d
Jeonghyun <limithyunii@gmail.com>
2024-09-21 21:49:10 +0900

    [level 1] Title: 소수 찾기, Time: 58.43 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    if (n < 2) return 0;
+
+    let isPrime = Array(n + 1).fill(true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (let i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    return isPrime.filter(Boolean).length;
+}
+
