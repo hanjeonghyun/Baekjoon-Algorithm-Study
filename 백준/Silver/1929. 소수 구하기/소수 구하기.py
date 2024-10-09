b4f6f411cfc9b3045920f038d31af3670f305354
Jeonghyun <limithyunii@gmail.com>
2024-10-09 14:11:52 +0900

    [Silver III] Title: 소수 구하기, Time: 272 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,15 @@
+# 에라토스테네스의 체 방법을 사용한 더 빠른 코드
+
 m, n = map(int, input().split())
 
-def primeChecker(num):
-    if num < 2: 
-        return False
-    return all(num % i != 0 for i in range(2, int(num ** 0.5) + 1)) 
+sieve = [True] * (n + 1)
+sieve[0] = sieve[1] = False
 
-for j in range(m, n + 1):
-    if primeChecker(j): 
-        print(j)
+for i in range(2, int(n ** 0.5) + 1):
+    if sieve[i]:
+        for j in range(i * i, n + 1, i):
+            sieve[j] = False
 
+for k in range(m, n + 1):
+    if sieve[k]:
+        print(k)
