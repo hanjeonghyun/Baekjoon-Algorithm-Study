adcbf4f28949774fb85eaed0d7c90fcc82a8b60c
Jeonghyun <limithyunii@gmail.com>
2024-10-17 23:00:50 +0900

    [level 1] Title: 문자열 나누기, Time: 28.99 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    let x = 0;
+    let x_count = 0;
+    let result = [];
+    
+    for (i = 0; i < s.length; i++) {
+        if (s[i] === s[x]) x_count++;
+        else x_count--;
+        if (x_count === 0) {
+            result.push(s.substring(x, i + 1));
+            x = i + 1;
+        }
+    }
+    return result.join('').length === s.length ? result.length : result.length + 1;
+}
